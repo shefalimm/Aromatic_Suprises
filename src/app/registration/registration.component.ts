01f93fbb13c800c69ff31075aebc4ae921e3b145
shefalimm <43922788+shefalimm@users.noreply.github.com>
2020-10-03 20:08:26 +0530

    validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  RegistrationForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.RegistrationForm = this.formBuilder.group({
+      fname:['', Validators.required],
+      lname:['', Validators.required],
+      email:['', Validators.required, Validators.email],
+      mnumber:['', Validators.required],
+      gender:['', Validators.required],
+      bday:['', Validators.required],
+      address:['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+     
+    })
+  }
+
+  get f(){
+    return this.RegistrationForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    if(this.RegistrationForm.invalid){
+      return;
+    }
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.RegistrationForm.value, null, 4));
+   
   }
 
 }
